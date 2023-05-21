b70b184f4fdc33d463f6599a790d60f1c7c3d24c
c <mustafasaeed5580@>
2023-05-21 19:32:27 +0300

    initial project . added empty home screen.

---
+++ android/app/src/main/kotlin/com/example/session_5_islami_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.session_5_islami_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:session_5_islami_project/Home_Screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+      },
+      initialRoute: HomeScreen.routeName,
+    );
+  }
+}
